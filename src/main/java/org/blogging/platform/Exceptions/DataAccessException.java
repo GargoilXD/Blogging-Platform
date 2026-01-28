4c9ac1758163b597206e0358d20372cd2acb9708
Gargoil <emmanueledusei.k@gmail.com>
2026-01-28 13:08:32 +0000

    Feature: DataAccessor to Represent DAOs

---
+++ src/main/java/org/blogging/platform/Exceptions/DataAccessException.java
@@ -0,0 +1,7 @@
+package org.blogging.platform.Exceptions;
+
+public class DataAccessException extends Exception {
+    public DataAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/org/blogging/platform/Interfaces/DataAccessor.java
+++ src/main/java/org/blogging/platform/Interfaces/DataAccessor.java
@@ -1,4 +1,24 @@
 package org.blogging.platform.Interfaces;
 
-public interface DataAccessor {
+import org.blogging.platform.Exceptions.DataAccessException;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DataAccessor<V, T extends DataTransporter> {
+    default Optional<V> getByID(long id) throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported");
+    }
+    default List<V> getAll() throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported");
+    }
+    default void save(T value) throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported");
+    }
+    default void update(T value) throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported");
+    }
+    default void delete(long ID) throws DataAccessException {
+        throw new UnsupportedOperationException("Not supported");
+    }
 }
